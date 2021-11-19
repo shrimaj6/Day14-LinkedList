79a5499eaea63bc0c6b7f2ffb0f67845402bac8b
Shrimaj <shrimaj.mehta60@gmail.com>
2021-11-19 17:00:57 +0530

    Added UC-10 Program.

--- Program.cs
+++ Program.cs
@@ -10,31 +10,22 @@ namespace LinkedList
             List<int> L1 = new List<int>();
             L1.Add(56);
             L1.Add(30);
-            L1.Add(40);
             L1.Add(70);
+            L1.Add(40);
 
-            foreach(var element in L1)
+            foreach (var element in L1)
             {
                 Console.WriteLine(element);
             }
 
-            Console.WriteLine("----------After----");
-            int size = L1.Count;
-            for(int i= 0; i<size; i++)
-            {
-                if (L1[i] == 40) ;
-                {
-                    L1.RemoveAt(i);
-                    break;
-                }
-                
-            }
-
-            Console.WriteLine("Size is " + L1.Count);
-            foreach(var element in L1)
+            Console.WriteLine("-------------After sorting -----------------");
+            L1.Sort();
+            foreach (var element in L1)
             {
                 Console.WriteLine(element);
             }
+
+            
             
         }
     }
