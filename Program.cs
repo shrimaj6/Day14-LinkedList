6c2382412ad638fa463464a115ec5a57507a103c
Shrimaj <shrimaj.mehta60@gmail.com>
2021-11-19 16:56:55 +0530

    Added UC-9 Program.

--- Program.cs
+++ Program.cs
@@ -10,6 +10,7 @@ namespace LinkedList
             List<int> L1 = new List<int>();
             L1.Add(56);
             L1.Add(30);
+            L1.Add(40);
             L1.Add(70);
 
             foreach(var element in L1)
@@ -17,24 +18,23 @@ namespace LinkedList
                 Console.WriteLine(element);
             }
 
-            Console.WriteLine("----------------");
+            Console.WriteLine("----------After----");
             int size = L1.Count;
-            for(int i = 0; i<size; i++)
+            for(int i= 0; i<size; i++)
             {
-                if(L1[i]== 30 )
+                if (L1[i] == 40) ;
                 {
-                    L1.Insert(i + 1, 40);
+                    L1.RemoveAt(i);
                     break;
                 }
-
+                
             }
 
+            Console.WriteLine("Size is " + L1.Count);
             foreach(var element in L1)
             {
                 Console.WriteLine(element);
             }
-                
-            
             
         }
     }
